2839dfe7ae883140e89469c622cac4a363041c20
Jesse Hernandez <jesseovr@gmail.com>
2023-10-04 14:08:05 -0500

    Update tailwind.config.ts
    spurs colors added to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ module.exports = {
     theme: {
         extend: {
             colors: {
+                spursTurq: '#00b2a9',
+                spursPink: '#ef426f',
+                spursOrange: '#ff8200',
                 centroGreen: '#ACDB6B',
                 centroYellow: '#FACB11',
                 centroBlue: '#1DB7BA',
